3a474ce90446eb4ee462f59b18209d9dab931770
Carlos <carlos.gil.berrocal@gmail.com>
2019-12-09 00:44:40 +0100

    Primer commit - Produccion

---
+++ public/js/socket.nuevo-ticket.js
@@ -0,0 +1,27 @@
+
+
+// comando para establecer la conexión 
+var socket = io();
+
+var label = $('#lblNuevoTicket');
+
+socket.on('connect', function() {
+    console.log('Conectado al servidor');
+});
+
+socket.on('disconnect', function() {
+    console.log('Conexión con el servidor interrumpida');
+})
+
+socket.on('estadoActual', function(resp){
+
+    label.text(resp.actual);
+})
+
+$('button').on('click', function(){
+
+    socket.emit('siguienteTicket', null, function(siguiente){
+
+        label.text(siguiente);
+    })
+})
